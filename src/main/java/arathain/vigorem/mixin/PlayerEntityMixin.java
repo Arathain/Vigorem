573bcf771b9d2a0acaba9f82aac01667886362c3
Arathain <arathainfarqoe@gmail.com>
2022-08-29 19:00:21 +0200

    Sanguine, vigorem.

---
+++ src/main/java/arathain/vigorem/mixin/PlayerEntityMixin.java
@@ -0,0 +1,25 @@
+package arathain.vigorem.mixin;
+
+import arathain.vigorem.VigoremComponents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Inject(method = "tick", at = @At("HEAD"))
+	public void neko$tick(CallbackInfo info) {
+		if(!world.isClient) {
+			this.getComponent(VigoremComponents.ANIMATION).serverTick();
+		}
+	}
+}
