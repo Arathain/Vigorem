ca2c7e8156ff2840eca90e35aa08b103a3bd8d02
Arathain <arathainfarqoe@gmail.com>
2022-09-25 15:54:06 +0200

    e

--- src/main/java/arathain/vigorem/anim/Keyframe.java
+++ src/main/java/arathain/vigorem/anim/Keyframe.java
@@ -3,15 +3,26 @@ package arathain.vigorem.anim;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.util.math.Vec3f;
 
-public class Keyframe {
+public final class Keyframe {
 	public final Easing easing;
 	public final Vec3f translation;
 	public final Vec3f rotation;
 	public final Vec3f offset;
 	public final Vec3f scale;
+	public final boolean override;
 
 	public final int frame;
 	public Keyframe(Easing easing, Vec3f translation, Vec3f rot, Vec3f scale, Vec3f offset, int frame) {
+		this.override = true;
+		this.easing = easing;
+		this.translation = translation;
+		this.rotation = rot;
+		this.scale = scale;
+		this.frame = frame;
+		this.offset = offset;
+	}
+	public Keyframe(Easing easing, Vec3f translation, Vec3f rot, Vec3f scale, Vec3f offset, int frame, boolean override) {
+		this.override = override;
 		this.easing = easing;
 		this.translation = translation;
 		this.rotation = rot;
