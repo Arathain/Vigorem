9e8dcd1a0a2751497051712abbe1b386d4d1ab11
Arathain <arathainfarqoe@gmail.com>
2023-04-30 12:01:56 +0200

    yippee

--- converterer.py
+++ converterer.py
@@ -1,16 +1,29 @@
 import json
 
-# Opening JSON file
+# Open
 f = open('anim_to_read.json')
 
-# returns JSON object as 
-# a dictionary
 data = json.load(f)
 conversion = {"Body" : "body", "RightArm" : "right_arm", "LeftArm" : "left_arm", "RightHand" : "right_hand", "LeftHand" : "left_hand", "Head" : "head", "RightLeg" : "right_leg", "LeftLeg" : "left_leg",}
 convertEasing = {"linear" : "LINEAR", "easeInSine" : "SINE_IN", "easeOutSine" : "SINE_OUT", "easeInOutSine" : "SINE_IN_OUT", "easeInQuad" : "QUAD_IN", "easeOutQuad" : "QUAD_OUT", "easeInOutQuad" : "QUAD_IN_OUT", "easeInQuart" : "QUARTIC_IN", "easeOutQuart" : "QUARTIC_OUT", "easeInOutQuart" : "QUARTIC_IN_OUT", "easeInQuint" : "QUINTIC_IN", "easeOutQuint" : "QUINTIC_OUT", "easeInOutQuint" : "QUINTIC_IN_OUT", "easeInBack" : "BACK_IN", "easeOutBack" : "BACK_OUT", "easeInOutBack" : "BACK_IN_OUT", "easeInExpo" : "EXPO_IN", "easeOutExpo" : "EXPO_OUT", "easeInOutExpo" : "EXPO_IN_OUT"}
 uberlist = []
-# Iterating through the json
-# list
+
+def getMolang(value):
+    return "new MoLangVec3fSupplier((math, query) -> " + str(value[2][0]) +", (math, query) -> " + str(value[2][1]) +", (math, query) -> " + str(value[2][2]) + ")"
+
+def getMolangDeg(value):
+    return "new MoLangVec3fSupplier((math, query) -> " + str(value[2][0]) +", (math, query) -> " + str(value[2][1]) +", (math, query) -> " + str(value[2][2]) + ", true)"
+
+def isMolang(value):
+    if 'math' in str(value[2][0]) or 'query' in str(value[2][0]):
+        return True
+    if 'math' in str(value[2][1]) or 'query' in str(value[2][1]):
+        return True
+    if 'math' in str(value[2][2]) or 'query' in str(value[2][2]):
+        return True
+    return False
+
+# the son of the J
 for i in data['animations']:
     animList = []
     for k in data['animations'][i]['bones']:
@@ -69,11 +82,9 @@ for i in data['animations']:
                 else:
                     frames[frameTimes[soup]] = [("scale", easings[soup], vecs[soup])]
         animList.append((conversion[k] if k in conversion else k, (frames)))
-        # if k in conversion:
-        #    print(conversion[k])
-        # print(frames)
+
     uberlist.append((animList, int(data['animations'][i]['animation_length']*20), str(i)))
-# Closing file
+# data extracted, you can now pray
 f.close()
 
 with open('export.txt', 'a') as f:
@@ -84,23 +95,34 @@ with open('export.txt', 'a') as f:
             for frame in range(len(jointAnim[1])):
                 key = list(jointAnim[1].keys())[frame]
                 value = list(jointAnim[1].values())[frame]
-                rot = "Vec3f.ZERO"
-                pos = "Vec3f.ZERO"
-                scale = "Vec3f.ZERO"
+                rot = "ProperVec3fSupplier.ZERO"
+                pos = "ProperVec3fSupplier.ZERO"
+                scale = "ProperVec3fSupplier.ZERO"
                 easing = "LINEAR"
                 for val in value:
                     match val[0]:
                         case "pos":
-                            pos = "new Vec3f(" + str(val[2][0]) +"f, " + str(val[2][1]) +"f, " + str(val[2][2]) + "f)"
+                            if isMolang(val):
+                                pos = getMolang(val)
+                            else:
+                                pos = "new ProperVec3fSupplier(" + str(val[2][0]) +"f, " + str(val[2][1]) +"f, " + str(val[2][2]) + "f)"
                             easing = val[1]
                         case "scale":
-                            scale = "new Vec3f(" + str(val[2][0]) +"f, " + str(val[2][1]) +"f, " + str(val[2][2]) + "f)"
+                            if isMolang(val):
+                                scale = getMolang(val)
+                            else:
+                                scale = "new ProperVec3fSupplier(" + str(val[2][0]) +"f, " + str(val[2][1]) +"f, " + str(val[2][2]) + "f)"
                             easing = val[1]
                         case "rot":
-                            rot = "deg(" + str(round(val[2][0], 2)) +"f, " + str(round(val[2][1], 2)) +"f, " + str(round(val[2][2], 2)) + "f)"
+                            if isMolang(val):
+                                rot = getMolangDeg(val)
+                            else:
+                                rot = "deg(" + str(round(val[2][0], 2)) +"f, " + str(round(val[2][1], 2)) +"f, " + str(round(val[2][2], 2)) + "f)"
                             easing = val[1]
-                f.write("cache.add(new Keyframe(Easing." + (convertEasing[easing] if easing in convertEasing else easing) + ", " + pos + ", " + rot + ", " + scale + ", " + "" + ("new Vec3f(0, -12, 0), " if jointAnim[0] == "body" else "Vec3f.ZERO, ") + str(round(key*20, 2 if frame != len(jointAnim[1])-1 else 0))+ "f));\n")
+                f.write("cache.add(new Keyframe(Easing." + (convertEasing[easing] if easing in convertEasing else easing) + ", " + pos + ", " + rot + ", " + scale + ", " + "" + ("new ProperVec3fSupplier(0, -12, 0), " if jointAnim[0] == "body" else "ProperVec3fSupplier.ZERO, ") + str(round(key*20, 2 if frame != len(jointAnim[1])-1 else 0))+ "f));\n")
             f.write(anim[2].upper() + ".put(\"" + jointAnim[0] + "\", new ArrayList<>(cache));\n")
             f.write("cache.clear();\n")
         f.write("Animations.put(id(\"" + anim[2] + "\"), () -> new Animation(" + str(anim[1]) + ", " + anim[2].upper() +"));\n")
     f.close()
+
+
--- src/main/java/arathain/vigorem/anim/MoLangVec3fSupplier.java
+++ src/main/java/arathain/vigorem/anim/MoLangVec3fSupplier.java
@@ -1,31 +1,40 @@
 package arathain.vigorem.anim;
 
 import arathain.vigorem.api.Vec3fSupplier;
+import net.minecraft.util.math.MathHelper;
 
 public class MoLangVec3fSupplier extends Vec3fSupplier {
 	private final ValueSupplier x, y, z;
 	private EntityQuery q;
+	private final boolean deg;
 	public MoLangVec3fSupplier(ValueSupplier x, ValueSupplier y, ValueSupplier z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.deg = false;
+	}
+	public MoLangVec3fSupplier(ValueSupplier x, ValueSupplier y, ValueSupplier z, boolean degrees) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.deg = degrees;
 	}
 	public void update(EntityQuery in) {
 		q = in;
 	}
 	@Override
 	public float getX() {
-		return x.get(MathsEngine.get(), q);
+		return x.get(MathsEngine.get(), q) * (deg ? MathHelper.PI/180 : 1);
 	}
 
 	@Override
 	public float getY() {
-		return y.get(MathsEngine.get(), q);
+		return y.get(MathsEngine.get(), q) * (deg ? MathHelper.PI/180 : 1);
 	}
 
 	@Override
 	public float getZ() {
-		return z.get(MathsEngine.get(), q);
+		return z.get(MathsEngine.get(), q) * (deg ? MathHelper.PI/180 : 1);
 	}
 
 	@Override
