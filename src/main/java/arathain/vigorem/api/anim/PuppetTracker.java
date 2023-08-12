5dd66c7455174a867f434159ca5a16fae5d0d2c5
Arathain <arathainfarqoe@gmail.com>
2023-08-12 15:28:54 +0200

    entity puppets

--- src/main/java/arathain/vigorem/api/anim/PuppetTracker.java
+++ src/main/java/arathain/vigorem/api/anim/PuppetTracker.java
@@ -9,6 +9,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3f;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -40,7 +41,7 @@ public class PuppetTracker {
 	}
 	/**
 	 * This one's a bit of a mess -
-	 * @param box the size of the box in question
+	 * @param box the size of the collision box in question
 	 * @param player the player we're using this on
 	 * @param initialOffset the offset (normally 0.75 y, where the body starts)
 	 * @param yaw player yaw
@@ -66,9 +67,41 @@ public class PuppetTracker {
 		}
 		return entities;
 	}
+	/**
+	 * This one's a bit of a mess -
+	 * @param box the size of the collision box in question
+	 * @param target the target entity - has to be an animated entity
+	 * @param initialOffset the offset (normally 0.75 y, where the body starts)
+	 * @param yaw entity yaw
+	 * @param precision how many tries should the method go through
+	 * @param precisionOffset a value between 0 and 1, limiting the scope of the animation. May be useful when movement only happens at a specific point between two ticks
+	 * @param rotationProviders the providers for rotation - should take in a single float value as the delta parameter, and return all of your rotations.
+	 * **/
+	@SafeVarargs
+	public final List<Entity> getCollided(Vec3d box, Entity target, Vec3d initialOffset, float yaw, int precision, float precisionOffset, Function<Float, Vec3f>... rotationProviders) {
+		List<Entity> entities = new ArrayList<>();
+		OrientedBox OBB = new OrientedBox(Vec3d.ZERO, box, 0, 0, 0);
+		for(int i = 1; i <= precision; i++) {
+			Vec3f[] rot = new Vec3f[rotationProviders.length];
+			for(int l = 0; l < rotationProviders.length; l++) {
+				rot[l] = rotationProviders[l].apply((i/precision) * (1-precisionOffset) + precisionOffset);
+			}
+			setRotations(rot);
+			Vec3d offset = getEntityOffset(target, initialOffset, yaw);
+			Vec3f rotation = rotatoProper(rot);
+			OBB.offset(offset).setRotation(rotation.getX(), rotation.getY(), rotation.getZ()).updateVertex();
+			entities.addAll(target.getWorld().getOtherEntities(target, target.getBoundingBox().expand(160, 160, 160), entity -> OBB.intersects(entity.getBoundingBox()) && !entities.contains(entity)));
+			OBB.resetOffset();
+		}
+		return entities;
+	}
 	public Vec3d getPlayerOffset(PlayerEntity player, Vec3d initialOffset, float yaw) {
 		Vec3d offset = this.getOffset(initialOffset);
 		return getOffsetVector(player, offset, yaw);
+	}
+	public Vec3d getEntityOffset(Entity entity, Vec3d initialOffset, float yaw) {
+		Vec3d offset = this.getOffset(initialOffset);
+		return getOffsetVector(entity, offset, yaw);
 	}
 	public static Vec3d rotateViaQuat(Vec3d rot, Quaternion quat) {
 		Quaternion q = quat.copy();
@@ -112,5 +145,8 @@ public class PuppetTracker {
 	}
 	public static Vec3d getOffsetVector(PlayerEntity player, Vec3d offset, float yaw) {
 		return player.getPos().add(offset.multiply(1, 1, -1).multiply(0.9375F).rotateY((180 - yaw) * 0.017453292F).multiply(Vigorem.getPlayerScale(player, 1)));
-	};
+	}
+	public static Vec3d getOffsetVector(Entity target, Vec3d offset, float yaw) {
+		return target.getPos().add(offset.multiply(1, 1, -1).rotateY((180 - yaw) * 0.017453292F));
+	}
 }
